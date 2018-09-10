68cde2dd678d65a453807c6dca67b35bf6f8f93b
Benjamin <rego_1994@hotmail.com>
2018-09-10 15:41:41 -0300

    Seeds and clases

---
+++ clases.py
@@ -0,0 +1,192 @@
+import datetime
+import csv
+
+class City:
+    def __init__(self, city):
+        self.city = city
+        self._hotel_cost = None
+        self.team = None
+
+        self.distances = {}  # dict with {[City] = distance (mi)}
+        self.flights = {}  # dict with {[City] = cost}
+
+    @property
+    def hotel_cost(self):
+        return self._hotel_cost
+
+    @hotel_cost.setter
+    def hotel_cost(self, cost):
+        self._hotel_cost = int(cost)
+
+    def add_distance(self, city, distance):
+        distance = int(distance)
+        if self == city or distance == 0:
+            return
+
+        if city not in self.distances:
+            self.distances[city] = distance
+
+        if self not in city.distances:
+            city.distances[self] = distance
+
+    def add_flight(self, to_city, cost):
+        cost = int(cost)
+        if self == to_city or cost == 0:
+            return
+
+        if to_city not in self.flights:
+            self.flights[to_city] = cost
+
+class Team:
+    def __init__(self, code):
+        self.name = None
+        self.code = code
+        self.arena = None
+        self.city = None
+        self.games = []
+
+    def add_game(self, game):
+        if game not in self.games:
+            self.games.append(game)
+            return True
+        return False
+
+    def set_city(self, city):
+        self.city = city
+        city.team = self
+
+class Channel:
+    def __init__(self, name):
+        self.name = name
+        self.games = []
+
+    def add_game(self, game):
+        if game not in self.games:
+            self.games.append(game)
+            return True
+        return False
+
+class Game:
+    def __init__(self, home, away, date, day, channel):
+        self.home = home
+        self.home.add_game(self)
+
+        self.away = away
+        self.away.add_game(self)
+
+        self.date = date
+        self.day = int(day)
+
+        self.channel = channel
+        if self.channel:
+            self.channel.add_game(self)
+
+class NBA:
+    def __init__(self):
+        self.cities = {}  # dict with {[NAME] = City}
+        self.channels = {}  # dict with {[NAME] = Channel}
+        self.teams = {}  # dict with {[CODE] = Team}
+        self.games = {}  # dict with {[DATE] = [Games]}
+
+    def pick_city(self, name):
+        if name not in self.cities:
+            self.cities[name] = City(name)
+        return self.cities[name]
+
+    def pick_channel(self, name):
+        if name not in self.channels:
+            self.channels[name] = Channel(name)
+        return self.channels[name]
+
+    def pick_team(self, code):
+        if code not in self.teams:
+            self.teams[code] = Team(code)
+        return self.teams[code]
+
+    def add_game(self, game):
+        if game.date not in self.games:
+            self.games[game.date] = []
+        self.games[game.date].append(game)
+        return True
+
+    def seed_locations(self, file):
+        with open(file) as csvfile:
+            lines = csv.DictReader(csvfile)
+            for line in lines:
+                team = self.pick_team(line["CODE"])
+                team.name = line["TEAM"]
+                team.arena = line["ARENA"]
+
+                city = self.pick_city(line["CITY"])
+                city.hotel_cost = line["HOTEL COST"]
+
+                team.set_city(city)
+
+    def seed_games(self, file):
+        with open(file) as csvfile:
+            lines = csv.DictReader(csvfile)
+            for line in lines:
+                channel_name = line["CHANNEL"]
+                channel = None
+                if channel_name != "X":
+                    channel = self.pick_channel(line["CHANNEL"])
+
+                away = self.pick_team(line["AWAY"])
+                home = self.pick_team(line["HOME"])
+
+                date = datetime.datetime.strptime(line["DATE"], "%m/%d").date()
+                if date.month >= 10:
+                    date = date.replace(year=2018)
+                else:
+                    date = date.replace(year=2019)
+
+                day = line["DAY"]
+
+                game = Game(home, away, date, day, channel)
+                self.add_game(game)
+
+    def seed_distances(self, file):
+        with open(file) as csvfile:
+            lines = csv.reader(csvfile)
+            teams = next(lines)[1:]  # el header
+
+            for idx1, line in enumerate(lines):
+                team = line[0]
+                index_team = teams.index(team)
+                distances = line[index_team + 2:]
+
+                from_city = self.teams[team].city
+
+                for idx2, distance in enumerate(distances):
+                    to_city = self.teams[teams[idx1 + idx2 + 1]].city
+                    from_city.add_distance(to_city, distance)
+
+    def seed_flight_costs(self, file):
+        with open(file) as csvfile:
+            lines = csv.reader(csvfile)
+            teams = next(lines)[1:]  # el header
+
+            for line in lines:
+                team = line[0]
+                costs = line[1:]
+
+                from_city = self.teams[team].city
+
+                for idx1, cost in enumerate(costs):
+                    if cost == 0 or not cost:
+                        continue
+
+                    to_city = self.teams[teams[idx1]].city
+                    from_city.add_flight(to_city, cost)
+
+
+if __name__ == '__main__':
+    nba = NBA()
+
+    print("Cargando seeds")
+    nba.seed_locations("datos/locations.csv")
+    nba.seed_games("datos/games.csv")
+    nba.seed_distances("datos/distances (mi & km).csv")
+    nba.seed_flight_costs("datos/flight costs.csv")
+    print("Seeds terminado")
+
